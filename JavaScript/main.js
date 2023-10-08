f00b1e007dd66d8e1570e504dc61ad5a1aefd556
Adella Ramadhan <145951591+daehokkim@users.noreply.github.com>
2023-10-08 23:18:31 +0700

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,77 @@
+//IF ELSE//
+/*var aku = "ganteng";
+if (aku == "jelek") {
+  console.log("Diterima");
+} else {
+  console.log("Kamu terlalu baik buat aku");
+}*/
+
+//NESTED IF//
+/*var usia = 15;
+
+if ((usia = 18)) {
+  if (usia < 12) {
+    console.log("Belum boleh masuk wahana");
+  } else {
+    console.log("Boleh masuk beberapa wahana");
+  }
+} else {
+  console.log("Boleh masuk semua wahana");
+}*/
+
+//SWITCH CASE//
+
+/*var cowo = "baik";
+
+switch (cowo) {
+  case "goodlooking":
+    console.log("jadian");
+    break;
+  case "goodrekening":
+    console.log("jadian");
+    break;
+  default:
+    console.log("Maaf ya aku fokus kuliah dulu");
+}*/
+
+//FOR STATEMENT//
+
+/*var a = ["HTML", "CSS", "JavaScript", "PHP"];
+var b;
+for (b in a) {
+  console.log("Aku bisa menggunakan bahasa " + a[b]);
+}*/
+
+//while do while//
+
+/*var balapan = confirm("Apakah kamu mau balapan?");
+var counter = 0;
+
+while (balapan) {
+  var jawab = confirm("Apakah kamu mau balapan?");
+  counter++;
+  if (jawab == false) {
+    balapan = false;
+  }
+}
+
+document.write("Balapan telah dilakukan sebanyak " + counter + " kali");
+*/
+
+/*var balapan = confirm("Apakah kamu mau balapan?");
+var counter = 0;
+
+do {
+  counter++;
+  balapan = confirm("Apakah kamu mau balapan?");
+} while (balapan);
+
+document.write("Balapan telah dilakukan sebanyak " + counter + " kali");
+*/
+
+//FUNCTION//
+
+/*function sayHi() {
+  console.log("Hi Gess");
+}
+sayHi();*/
